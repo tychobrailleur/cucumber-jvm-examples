9711f32ddc4679ec481758cb58e25003f739d2e9
Sébastien Le Callonnec <sebastien@weblogism.com>
2015-02-28 17:25:14 +0000

    Upgrade to latest cucumber-jvm version.

--- java-example/src/it/java/com/weblogism/cucumberjvm/javaexample/RunCukesIT.java
+++ java-example/src/it/java/com/weblogism/cucumberjvm/javaexample/RunCukesIT.java
@@ -3,8 +3,9 @@ package com.weblogism.cucumberjvm.javaexample;
 import org.junit.runner.RunWith;
 
 import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = { "json:target/report.json" })
+@CucumberOptions(plugin = "json:target/report.json")
 public class RunCukesIT {
 }
