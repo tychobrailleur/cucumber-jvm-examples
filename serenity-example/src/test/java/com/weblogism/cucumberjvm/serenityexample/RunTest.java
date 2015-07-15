5c89e9cc71acefd916ebc6fc4aa075b3b7ed03d7
Sébastien Le Callonnec <sebastien@weblogism.com>
2015-07-15 07:59:30 +0100

    First serenity example commit.

---
+++ serenity-example/src/test/java/com/weblogism/cucumberjvm/serenityexample/RunTest.java
@@ -0,0 +1,11 @@
+package com.weblogism.cucumberjvm.serenityexample;
+
+import org.junit.runner.RunWith;
+
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import cucumber.api.CucumberOptions;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(plugin = "json:target/report.json")
+public class RunTest {
+}
